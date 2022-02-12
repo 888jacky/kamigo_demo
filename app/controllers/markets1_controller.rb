f7ad8c22a4a61435fb7eabbcfd61ae8b7d0e03e6
888jacky <99524912+888jacky@users.noreply.github.com>
2022-02-12 21:54:11 +0800

    0212測試0028

--- app/controllers/markets1_controller.rb
+++ app/controllers/markets1_controller.rb
@@ -5,19 +5,19 @@ class Markets1Controller < ApplicationController
           "圖片": "https://live.staticflickr.com/65535/51877543975_7883540684_n.jpg",
           "攤商": "燕巢阿忠水果行",
           "服務": "新鮮水果禮盒\\n新鮮水果籃\\n外送服務",
-          "連結": "https://momo.dm/R7YqRI",
+          "電話": "手機0912-127-246\\n電話(06)331-6678\\n電話(06)331-0780",
         },
         {
           "圖片": "https://live.staticflickr.com/65535/51877543975_7883540684_n.jpg",
           "攤商": "燕巢阿忠水果行",
           "服務": "新鮮水果禮盒\\n新鮮水果籃\\n外送服務",
-          "連結": "https://momo.dm/R7YqRI",
+          "電話": "手機0912-127-246\\n電話(06)331-6678\\n電話(06)331-0780",
         },
         {
           "圖片": "https://live.staticflickr.com/65535/51877543975_7883540684_n.jpg",
           "攤商": "燕巢阿忠水果行",
           "服務": "新鮮水果禮盒\\n新鮮水果籃\\n外送服務",
-          "連結": "https://momo.dm/R7YqRI",
+          "電話": "手機0912-127-246\\n電話(06)331-6678\\n電話(06)331-0780",
         },
     ]
 
@@ -25,7 +25,7 @@ class Markets1Controller < ApplicationController
 
     if @keyword.present?
         @markets1.filter! do |market|
-            market[:名稱].include? @keyword
+            market[:攤商].include? @keyword
       end
     end
     @markets1 = @markets1.first(12)
