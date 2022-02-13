1c5331e4fc7e5271b5e7d47da8e391cf7f5054dc
888jacky <99524912+888jacky@users.noreply.github.com>
2022-02-13 17:16:59 +0800

    0213測試0045

--- config/routes.rb
+++ config/routes.rb
@@ -22,7 +22,7 @@ Rails.application.routes.draw do
 
   # menu
   get "menu", to: "home#menu"
-  get "攤商查詢", to: "home#markets1"
+  get "攤商查詢", to: "home#mark1"
   get "阿忠水果行", to: "markets1#index"
   
   # 查天氣
