a94ea405d927cbe25c3c22050a1193cb7a461b49
888jacky <99524912+888jacky@users.noreply.github.com>
2022-02-21 17:23:27 +0800

    0221雜貨店0107

--- app/controllers/markets9_controller.rb
+++ app/controllers/markets9_controller.rb
@@ -2,9 +2,15 @@ class Markets9Controller < ApplicationController
     def index
       @markets9 = [
         {
-          "圖片": "https://live.staticflickr.com/65535/51886921923_e0ddc22888_b.jpg",
+          "圖片": "https://live.staticflickr.com/65535/51895242360_5baa3bedca_b.jpg",
           "攤商": "新喜珍肉舖",
-          "服務": " ",
+          "服務": "泡菜香腸\n火鍋肉片\n烤肉片\n自製豬油\n高粱香腸",
+          "電話": "手機0981917078",
+        },
+        {
+          "圖片": "https://live.staticflickr.com/65535/51894589126_a6c89d857e_c.jpg",
+          "攤商": "新喜珍肉舖",
+          "服務": "泡菜香腸\n火鍋肉片\n烤肉片\n自製豬油\n高粱香腸",
           "電話": "手機0981917078",
         },
     ]
